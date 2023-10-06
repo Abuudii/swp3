358120646bb0bb9deb5a268649c12d1a41658bac
Abdalla <Abdalla.nada@student.htldornbirn.at>
2023-10-06 09:30:58 +0200

    Package basics mit bsp erstellt

--- src/at/ana/basic/Bankomat_Beispiel.java
+++ src/at/ana/basic/Bankomat_Beispiel.java
@@ -3,9 +3,9 @@ package at.ana.basic;
 import java.util.Scanner;
 
 public class Bankomat_Beispiel {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double Kontostand= 0;
+        double Kontostand = 0;
         Scanner scanner = new Scanner(System.in); // Für das Einlesen der eingabe
 
         System.out.println("1.Einzahlen");
@@ -13,31 +13,32 @@ public class Bankomat_Beispiel {
         System.out.println("3.Kontostand");
         System.out.println("4.Beenden");
 
-        while()
-        {
-            int Auswahl= scanner.nextInt();
+
+        int Auswahl;
+        do {
+            Auswahl = scanner.nextInt();
 
             switch (Auswahl) {
                 case 1:
                     System.out.println("Wie viel Geld wollen Sie einzahlen?");
-                    double Einzahlung= scanner.nextDouble();
-                    Kontostand= Kontostand+Einzahlung;
-                    System.out.println("Ihre "+Einzahlung+ " € war erfolgreich");
+                    double Einzahlung = scanner.nextDouble();
+                    Kontostand = Kontostand + Einzahlung;
+                    System.out.println("Ihre " + Einzahlung + " € war erfolgreich");
                     break;
                 case 2:
                     System.out.println("Wie viel Geld wollen Sie abheben?");
                     double Auszahlung = scanner.nextDouble();
-                    Kontostand=Kontostand-Auszahlung;
-                    System.out.println("Ihre Auszahlung"+Auszahlung+ "€ war erfolgreich");
+                    Kontostand = Kontostand - Auszahlung;
+                    System.out.println("Ihre Auszahlung" + Auszahlung + "€ war erfolgreich");
                     break;
                 case 3:
-                    System.out.println("Ihr Kontostand betraegt "+ Kontostand);
+                    System.out.println("Ihr Kontostand betraegt " + Kontostand + "€");
                     break;
                 case 4:
 
 
             }
-        }
+        } while (Auswahl != 4);
 
 
     }
