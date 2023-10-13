cc1d28ca200ee0a47654d538803ae0a7ec75460e
Abdalla <Abdalla.nada@student.htldornbirn.at>
2023-10-13 09:23:59 +0200

    Package basics mit bsp erstellt

--- src/at/ana/basic/Ceaserverschluesselung.java
+++ src/at/ana/basic/Ceaserverschluesselung.java
@@ -1,16 +1,24 @@
 package at.ana.basic;
 
 public class Ceaserverschluesselung {
-    public static void main(String[] args) {
+
+    public static void main (String[] args){
         String result = encrypt(1,"hallo");
+        System.out.println(result);
     }
 
-    public static String encrypt(int shift, stringToEncrypt){
+
+    public static String encrypt(int shift, String stringToEncrypt){
+        String encyptedString = "";
         char[] inputData = stringToEncrypt.toCharArray();
 
-        return "";
+        for(int i=0; i<inputData.length; i++){
+            int  letter = inputData[i] + shift ;
+            System.out.println("newLetter:" + Character.toString(letter));
+            encyptedString += Character.toString(letter);
+        }
+        return encyptedString;
     }
-    //noch nicht fertig
 
 }
 
