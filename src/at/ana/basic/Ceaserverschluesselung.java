575ab90a902b970315fcf71e757e3e67e2846dd2
Abdalla <Abdalla.nada@student.htldornbirn.at>
2023-10-18 08:43:35 +0200

    Package basics mit bsp erstellt

--- src/at/ana/basic/Ceaserverschluesselung.java
+++ src/at/ana/basic/Ceaserverschluesselung.java
@@ -1,24 +1,30 @@
 package at.ana.basic;
 
 public class Ceaserverschluesselung {
-
-    public static void main (String[] args){
-        String result = encrypt(1,"hallo");
+    public static void main(String[] args) {
+        String result = encrypt(1, "zzz");
         System.out.println(result);
     }
 
-
-    public static String encrypt(int shift, String stringToEncrypt){
-        String encyptedString = "";
+    public static String encrypt(int shift, String stringToEncrypt) {
+        String encryptedString = "";
         char[] inputData = stringToEncrypt.toCharArray();
 
-        for(int i=0; i<inputData.length; i++){
-            int  letter = inputData[i] + shift ;
-            System.out.println("newLetter:" + Character.toString(letter));
-            encyptedString += Character.toString(letter);
+        for (int i = 0; i < inputData.length; i++) {
+            char letter = inputData[i];
+
+            if (Character.isLetter(letter)) {
+                char base = Character.isUpperCase(letter) ? 'A' : 'a';
+                int shifted = (letter - base + shift) % 26;
+                if (shifted < 0) {
+                    shifted += 26;
+                }
+                encryptedString += (char) (base + shifted);
+            } else {
+                encryptedString += letter;
+            }
         }
-        return encyptedString;
+
+        return encryptedString;
     }
-
 }
-
---
+++ src/at/ana/basic/StringHelper.java
@@ -0,0 +1,30 @@
+package at.ana.basic;
+
+public class StringHelper{
+
+    public static void main(String[] args) {
+        countLetters("Hans", 'a');
+    }
+    public static boolean isPalindrome(String text){
+
+        return false;
+    }
+
+    public static int countLetters(String text, char search){
+        //Hans
+        char[] textArray = text.toCharArray();
+        int count = 0;
+
+        //0 1 2 3
+        //H a n s
+
+        for (int i=0; i<textArray.length; i++){
+
+            if(textArray[i]==search){
+                count++;
+            }
+        }
+
+        return -1;
+    }
+}
